5fcb0a879e936cafe0e137e7af03dec9249e386f
Thomas Choquet <tchoquet@student.42tokyo.jp>
2024-11-13 16:46:01 +0900

    working on Linux

--- src/dlLoad_unix.c
+++ src/dlLoad_unix.c
@@ -7,3 +7,20 @@
  * ---------------------------------------------------
  */
 
+#include "dlLoad/dlLoad.h"
+#include <dlfcn.h>
+
+DlHandle dlLoad(const char* path)
+{
+    return dlopen(path, RTLD_NOW | RTLD_GLOBAL);
+}
+
+void* getSym(DlHandle handle, const char* name)
+{
+    return dlsym(handle, name);
+}
+
+int dlFree(DlHandle handle)
+{
+    return dlclose(handle);
+}
