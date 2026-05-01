262e4d0a0a500aeb6ae554e1a0b5f946a609c173
Thomas Choquet <semoir.dense-0h@icloud.com>
2026-05-01 23:20:52 +0900

    Fix Linux dlLoad default library lookup

--- tests/testLibs/testLib5.cpp
+++ tests/testLibs/testLib5.cpp
@@ -16,13 +16,22 @@ int dyload_function()
     using Func = int (*)(void);
     
     Func f = (Func)getSym(DL_DEFAULT, "function");
+    if (f == nullptr)
+        return -1;
+
     return f();
 }
 
 int dyload_function2()
 {
     using Func = int (*)(void);
-    
-    Func f = (Func)getSym(DL_DEFAULT, "function2");
-    return f();
+
+    DlHandle handle = dlLoad(TEST_LIB_1_FILE);
+    if (handle == nullptr)
+        return -1;
+
+    Func f = (Func)getSym(handle, "function2");
+    int result = (f != nullptr) ? f() : -1;
+    dlFree(handle);
+    return result;
 }
